4148c1ef06438ee85d1887f89887abd8eed53373
Stefano Picchiotti <stefano_picchiotti@goodyear.com>
2019-01-28 08:17:59 +0100

    Minor edits to clean-up the code.

--- sketch.js
+++ sketch.js
@@ -17,37 +17,35 @@ function draw(){
 for (let i=0; i<slider.value(); i++){
   let prevx = x ;
   let prevy = y ;
-    let n = 2*i+1;
-    let radius = 100 * (4 / (n * PI)) ;
-    x += radius * cos(n*time) ;
-    y += radius * sin(n*time) ;
-     ;
+  let n = 2*i+1;
+  let radius = 100 * (4 / (n * PI)) ;
+  x += radius * cos(n*time) ;
+  y += radius * sin(n*time) ;
 
-    stroke(255, 100) ;
-    noFill() ;
-    ellipse(prevx,prevy, 2*radius) ;
+  stroke(255, 100) ;
+  noFill() ;
+  ellipse(prevx,prevy, 2*radius) ;
 
-    fill(255);
-    stroke(255);
-    //ellipse(x,y,8) ;
-    line(prevx,prevy,x,y) ;
-
-
+  fill(255);
+  stroke(255);
+  line(prevx,prevy,x,y) ;
 }
+
 wave.unshift(y)
 
 translate(300,0);
 line(x-300,y,0,wave[0]);
 
-    beginShape();
-    noFill();
-    for (let i=0; i<wave.length; i++){
-      vertex(i,wave[i]);
-    }
-    endShape()
+beginShape();
+noFill();
+for (let i=0; i<wave.length; i++){
+  vertex(i,wave[i]);
+}
+endShape()
 
-    time += 0.1 ;
-    if (wave.length > 600) {
-      wave.pop() ;
-    }
+time += 0.1 ;
+if (wave.length > 600) {
+  wave.pop() ;
+}
+
 }
