014fa0f9e9261ee37f2851e98cb35d5d2328b921
Stefano Picchiotti <stefano_picchiotti@goodyear.com>
2019-01-28 09:21:44 +0100

    Drawing path using epicycles

--- sketch.js
+++ sketch.js
@@ -1,51 +1,69 @@
-let time = 0 ;
-let wave = [] ;
-let slider ;
+let x = [];
+let y = [];
+let fourierX;
+let fourierY;
+let time = 0;
+let path = [];
 
 function setup() {
-    createCanvas(800,600) ;
-    slider = createSlider(1, 100, 1) ;
+  createCanvas(800, 600);
+  const skip = 8;
+  for (let i = 0; i < drawing.length; i += skip) {
+    x.push(drawing[i].x);
+    y.push(drawing[i].y);
+  }
+  fourierX = dft(x);
+  fourierY = dft(y);
+
+  fourierX.sort((a, b) => b.amp - a.amp);
+  fourierY.sort((a, b) => b.amp - a.amp);
 }
 
-function draw(){
-  background(0) ;
-  translate(200,250) ;
+function epiCycles(x, y, rotation, fourier) {
+  for (let i = 0; i < fourier.length; i++) {
+    let prevx = x;
+    let prevy = y;
+    let freq = fourier[i].freq;
+    let radius = fourier[i].amp;
+    let phase = fourier[i].phase;
+    x += radius * cos(freq * time + phase + rotation);
+    y += radius * sin(freq * time + phase + rotation);
 
-  let x = 0;
-  let y = 0;
-
-for (let i=0; i<slider.value(); i++){
-  let prevx = x ;
-  let prevy = y ;
-  let n = 2*i+1;
-  let radius = 100 * (4 / (n * PI)) ;
-  x += radius * cos(n*time) ;
-  y += radius * sin(n*time) ;
-
-  stroke(255, 100) ;
-  noFill() ;
-  ellipse(prevx,prevy, 2*radius) ;
-
-  fill(255);
-  stroke(255);
-  line(prevx,prevy,x,y) ;
+    stroke(255, 100);
+    noFill();
+    ellipse(prevx, prevy, radius * 2);
+    stroke(255);
+    line(prevx, prevy, x, y);
+  }
+  return createVector(x, y);
 }
 
-wave.unshift(y)
+function draw() {
+  background(0);
 
-translate(300,0);
-line(x-300,y,0,wave[0]);
+  let vx = epiCycles(width / 2 + 100, 100, 0, fourierX);
+  let vy = epiCycles(100, height / 2 + 100, HALF_PI, fourierY);
+  let v = createVector(vx.x, vy.y);
+  path.unshift(v);
+  line(vx.x, vx.y, v.x, v.y);
+  line(vy.x, vy.y, v.x, v.y);
 
-beginShape();
-noFill();
-for (let i=0; i<wave.length; i++){
-  vertex(i,wave[i]);
-}
-endShape()
+  beginShape();
+  noFill();
+  for (let i = 0; i < path.length; i++) {
+    vertex(path[i].x, path[i].y);
+  }
+  endShape();
 
-time += 0.1 ;
-if (wave.length > 600) {
-  wave.pop() ;
-}
+  const dt = TWO_PI / fourierY.length;
+  time += dt;
 
+  if (time > TWO_PI) {
+    time = 0;
+    path = [];
+  }
+
+  // if (wave.length > 250) {
+  //   wave.pop();
+  // }
 }
